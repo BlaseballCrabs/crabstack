89d55a5ba5d0e5d1f82c1d161c641d5501bb9572
leo60228 <leo@60228.dev>
2021-10-19 14:13:42 -0400

    allow PutObjectAcl

--- iam.tf
+++ iam.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "frontend_deploy_policy" {
   }
 
   statement {
-    actions   = ["s3:PutObject", "s3:GetObject", "s3:DeleteObject"]
+    actions   = ["s3:PutObject", "s3:PutObjectAcl", "s3:GetObject", "s3:DeleteObject"]
     resources = ["${aws_s3_bucket.frontend_bucket.arn}/*"]
     effect    = "Allow"
   }
